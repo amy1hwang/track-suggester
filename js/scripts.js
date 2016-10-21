9f8f13e2f295bedc0bf052e027fc646fe774b5b7
Amy Hwang <amyhwang1218@gmail.com>
2016-10-21 15:42:09 -0700

    add branching so submitted will not fadein unless all the questions are answered

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ var add = function(number1, number2, number3, number4, number5) {
   return number1 + number2 + number3 + number4 + number5;
 };
 
-var output = function(learn, build, environment, reason, lordOfTheRing, result) {
- if (isNaN(learn) || isNaN(build) || isNaN(environment) || isNaN(reason) || isNaN(lordOfTheRing)) {
+var output = function(name, learn, build, environment, reason, lordOfTheRing, result) {
+ if (name === "" || isNaN(learn) || isNaN(build) || isNaN(environment) || isNaN(reason) || isNaN(lordOfTheRing)) {
    alert("Answer all the questions to find out!")
  } else if (result >= 5 && result < 9) {
     return ("C#/.NET");
@@ -31,8 +31,14 @@ $(document).ready(function() {
     var name = $("input#name").val();
 
     $(".yourname").text(name);
-    $(".output").text(output(learn, build, environment, reason, lordOfTheRing, result));
-    $(".submitted").fadeIn();
+    $(".output").text(output(name, learn, build, environment, reason, lordOfTheRing, result));
+console.log(result)
+
+    if (isNaN(result) || name === "") {
+      $(".submitted").hiddne();
+    } else {
+      $(".submitted").fadeIn();
+    }
 
  });
 });
